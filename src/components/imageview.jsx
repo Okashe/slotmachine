a753711b421827a0f4d56e995ecd8f19138ebdba
Okashe <okashabdi88@gmail.com>
2024-01-28 02:59:00 -0600

    design and functionality improved

--- src/components/imageview.jsx
+++ src/components/imageview.jsx
@@ -14,6 +14,8 @@
 */
 import { useEffect, useState } from "react"
 import { fruits } from "../data/fruits"
+import {LifebuoyIcon } from "@heroicons/react/24/solid"
+import {ArrowPathIcon} from "@heroicons/react/24/solid"
 import '../index.css'
 
 
@@ -96,77 +98,108 @@ import '../index.css'
 
     const images = fruits; // Assuming fruits is an array of image URLs
 
-    const [bet, setBet] = useState(0);
-    const [amountWon, setAmountWon] = useState(0);
-    const [totalWon, setTotalWon] = useState(0);
-    const [image1, setImage1] = useState(images[0])
-    const [image2, setImage2] = useState(images[1])
-    const [image3, setImage3] = useState(images[2])
+    const [bet, setBet] = useState(parseFloat(localStorage.getItem("bet")) || 0);
+    const [amountWon, setAmountWon] = useState(parseFloat(localStorage.getItem("amountWon"))||0);
+    const [totalWon, setTotalWon] = useState(parseFloat(localStorage.getItem("totalWon"))||0);
+    const [image1, setImage1] = useState(localStorage.getItem("image1")||images[0])
+    const [image2, setImage2] = useState(localStorage.getItem("image2")||images[1])
+    const [image3, setImage3] = useState(localStorage.getItem("image3")||images[2])
     const [isSpinning, setIsSpinning] = useState(false);
+    const [info, setInfo] = useState('')
 
     
-   
+    useEffect(() => {
+        localStorage.setItem("bet", bet.toString());
+        localStorage.setItem("amountWon", amountWon.toString())
+        localStorage.setItem("totalWon", totalWon.toString());
+        localStorage.setItem("image1",image1)
+        localStorage.setItem("image2",image2)
+        localStorage.setItem("image3",image3)
+      }, [bet,amountWon, totalWon, image1, image2,image3]);
 
     const spin = () => {
-        setIsSpinning(true);
+        if(bet===0){
+           const message =  "You've to bet before you can spin!"
+           setInfo(message)
+        }else{
+            setInfo('')
+            setIsSpinning(true);
 
        
-        const randomIndex = () => Math.floor(Math.random() * images.length);
-
-        const index1 = randomIndex();
-        const index2 = randomIndex();
-        const index3 = randomIndex();
-
-        const image1 = images[index1];
-        const image2 = images[index2];
-        const image3 = images[index3];
-
-        // Set the displayed images
-        setImage1(image1);
-        setImage2(image2);
-        setImage3(image3);
-
-        let amountWon = 0;
-
-        if (image1=== image2 && image1 === image3) {
-            amountWon = 3 * bet;
-        } else if (image1 === image2 || image1 === image3 || image2 === image3) {
-            amountWon = 2 * bet;
-        }else{
-            amountWon =0;
+            const randomIndex = () => Math.floor(Math.random() * images.length);
+    
+            const index1 = randomIndex();
+            const index2 = randomIndex();
+            const index3 = randomIndex();
+    
+            const image1 = images[index1];
+            const image2 = images[index2];
+            const image3 = images[index3];
+    
+            // Set the displayed images
+            setImage1(image1);
+            setImage2(image2);
+            setImage3(image3);
+    
+            let amountWon = 0;
+    
+            if (image1=== image2 && image1 === image3) {
+                amountWon = 3 * bet;
+            } else if (image1 === image2 || image1 === image3 || image2 === image3) {
+                amountWon = 2 * bet;
+            }else{
+                amountWon =0;
+            }
+    
+            setAmountWon(amountWon);
+            setTotalWon(totalWon + amountWon);
+    
+             // Stop spinning after a delay (adjust as needed)
+             setTimeout(() => {
+                setIsSpinning(false);
+            }, 3000);
         }
-
-        setAmountWon(amountWon);
-        setTotalWon(totalWon + amountWon);
-
-         // Stop spinning after a delay (adjust as needed)
-         setTimeout(() => {
-            setIsSpinning(false);
-        }, 3000); // Change the delay duration as needed
     };
 
-   
+   const reset =()=>{
+    setBet(0)
+    setAmountWon(0)
+    setTotalWon(0)
+    setImage1(images[0])
+    setImage2(images[1])
+    setImage3(images[2])
+    setIsSpinning(false)
+   }
   
 return (
       <div className="container">
-        <div className={`images ${isSpinning ? "spin" : ""}`}>
+        <div className="images">
             {/* <img className="" src={getRandomElement(randomImages.IMAGE1)} alt="Random Image 1" />
             <img src={getRandomElement(randomImages.IMAGE2)} alt="Random Image 2" />
             <img src={getRandomElement(randomImages.IMAGE3)} alt="Random Image 3" /> */}
-            <img  src={image1} alt="Image 1" />
-            <img  src={image2} alt="Image 2" />
-            <img  src={image3} alt="Image 3" />
+            <img className={`imgOne ${isSpinning ? "spin" : ""}`} src={image1} alt="Image 1" />
+            <img className={`imageTwo ${isSpinning ? "spin" : ""}`} src={image2} alt="Image 2" />
+            <img className={`imageThree ${isSpinning ? "spin" : ""}`} src={image3} alt="Image 3" />
         </div>
          {/* Input for bet amount */}
-         <input
+        <div>
+         <p>How much are you betting?</p>
+         <span>$</span><input
                 type="number"
-                value={bet}
+                value={bet>=0?bet:0}
                 onChange={(e) => setBet(parseFloat(e.target.value))}
             />
+            <p className="info">{info}</p>
+        </div> 
         
-        <div className="btn-action">
-            <button className="spin-button" onClick={spin}>Spin</button>
-            <button className="spin-button" >Reset</button>
+        <div className="btn-action">spin
+            <button className="spin-button" onClick={spin}>
+                <LifebuoyIcon color="blue" className="spin-icon"/>
+            </button>
+            
+            <button className="spin-button" onClick={reset} >
+                <ArrowPathIcon color="blue" className="spin-icon"/>
+            </button>reset
         </div>
         <div className="resultDisplay">
             <p className="result">
